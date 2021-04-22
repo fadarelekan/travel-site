5cba6749b6d13da9c33cb55a49d1b92c4882c5ac
fadare olalekan <fadareisaac@yahoo.com>
2021-04-22 20:44:21 +0100

    commit in reveal-on-scroll branch

--- assets/scripts/App.js
+++ assets/scripts/App.js
@@ -1,6 +1,9 @@
 import '../styles/styles.css'
 import MobileMenu from './modules/MobileMenu'
+import RevealOnScroll from './modules/RevealOnScroll'
 alert ("Hello there , click okay if u can see this !!!")
+
+let revealOnScroll= new RevealOnScroll()
 
 let mobileMenu=new MobileMenu();
 
