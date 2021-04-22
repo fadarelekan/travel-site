1026f3de625665c1515e4c4e2ff2f9a7f7fea3ac
fadare olalekan <fadareisaac@yahoo.com>
2021-04-22 12:47:25 +0100

    commit in header branch

--- assets/scripts/App.js
+++ assets/scripts/App.js
@@ -1,6 +1,10 @@
 import '../styles/styles.css'
+import MobileMenu from './modules/MobileMenu'
 alert ("Hello there , click okay if u can see this !!!")
+
+let mobileMenu=new MobileMenu();
 
 if (module.hot){
     module.hot.accept()
 }
+
