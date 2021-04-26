68c0a39df412c37dd80af2e255e139be437d6c20
fadare olalekan <fadareisaac@yahoo.com>
2021-04-26 15:36:03 +0100

    commit for overlay

--- assets/scripts/App.js
+++ assets/scripts/App.js
@@ -2,14 +2,28 @@ import '../styles/styles.css'
 import MobileMenu from './modules/MobileMenu'
 import RevealOnScroll from './modules/RevealOnScroll'
 import StickyHeader from './modules/StickyHeader'
-import Overlay from './modules/Overlay'
 alert ("Hello there , click okay if u can see this !!!")
 
-new Overlay()
+
 let stickyHeader = new StickyHeader()
 let revealOnScroll= new RevealOnScroll()
 
 let mobileMenu=new MobileMenu();
+let overlay
+
+document.querySelectorAll(".open-overlay").forEach(el => {
+    el.addEventListener("click", e =>{
+        e.preventDefault()
+        if(typeof overlay =="undefined"){
+            import('./modules/Overlay').then(x => {
+                overlay=new x.default()
+                setTimeout(()=> overlay.openTheOverlay(),20)
+               }).catch(()=> console.log("There was a problem"))
+        } else {
+            overlay.openTheOverlay()
+        }
+    })
+})
 
 if (module.hot){
     module.hot.accept()
